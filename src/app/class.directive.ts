b5066fe405ef51f8fd691eea63a6a27d1307874f
Hani <ummehanimohammed9@gmail.com>
2023-03-28 02:40:35 +0530

    Binding properties to Attributes

--- src/app/class.directive.ts
+++ src/app/class.directive.ts
@@ -1,12 +1,19 @@
-import { Directive, ElementRef } from '@angular/core';
+import { Directive, ElementRef, Input } from '@angular/core';
 
 @Directive({
   selector: '[appClass]'
 })
 export class ClassDirective {
-
-  constructor(private element: ElementRef) { 
-    this.element.nativeElement.style.color = 'red'
-  }
+  
+  constructor(private element: ElementRef) { }
+    @Input('appClass') set classnames(classObj: any) {
+      for(let key in classObj) {
+        if(classObj[key]) {
+          this.element.nativeElement.classList.add(key)
+        } else {
+          this.element.nativeElement.classList.remove(key)
+        }
+      }
+    }
 
 }
